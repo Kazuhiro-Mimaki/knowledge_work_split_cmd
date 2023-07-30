69411657264656b2315765b7c8c632b45f13f86d
Kazuhiro-Mimaki <godblessu0405@gmail.com>
2023-07-30 11:27:55 +0900

    feat: parse "l"

--- main.go
+++ main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"flag"
+	"fmt"
+)
 
 func main() {
-    fmt.Println("Hello, World!")
+    var (
+        l int
+    )
+
+    flag.IntVar(&l, "l", 0, "line_count")
+
+    flag.Parse()
+
+    err := validateL(l)
+    if err != nil {
+        panic(err)
+    }
+
+    fmt.Println("pass")
+}
+
+func validateL(l int) (err error) {
+    if l < 0 {
+        return errors.New("validation error of l")
+    }
+    return nil
 }
