d1df82fa2653901b5f09b7c2e82afac5bd98d7d7
Kazuhiro-Mimaki <godblessu0405@gmail.com>
2023-08-04 05:46:00 +0900

    feat: mode test

---
+++ utils/mode_test.go
@@ -0,0 +1,55 @@
+package utils
+
+import (
+	"testing"
+)
+
+func TestMode(t *testing.T) {
+	t.Run("lモード", func(t *testing.T) {
+		got := Mode(1, 0, 0)
+		want := "l"
+		if got != want {
+			t.Errorf("NewFilenameGenerator() == %v, want %s", got, want)
+		}
+	})
+
+	t.Run("nモード", func(t *testing.T) {
+		got := Mode(0, 1, 0)
+		want := "n"
+		if got != want {
+			t.Errorf("NewFilenameGenerator() == %v, want %s", got, want)
+		}
+	})
+
+	t.Run("bモード", func(t *testing.T) {
+		got := Mode(0, 0, 1)
+		want := "b"
+		if got != want {
+			t.Errorf("NewFilenameGenerator() == %v, want %s", got, want)
+		}
+	})
+
+	t.Run("引数なし", func(t *testing.T) {
+		got := Mode(0, 0, 0)
+		want := "noArgs"
+		if got != want {
+			t.Errorf("NewFilenameGenerator() == %v, want %s", got, want)
+		}
+	})
+
+	t.Run("不正な入力(2つの入力値が存在する)", func(t *testing.T) {
+		got := Mode(0, 1, 1)
+		want := ""
+		if got != want {
+			t.Errorf("NewFilenameGenerator() == %v, want %s", got, want)
+		}
+	})
+
+	t.Run("不正な入力(全て入力値が存在する)", func(t *testing.T) {
+		got := Mode(1, 1, 1)
+		want := ""
+		if got != want {
+			t.Errorf("NewFilenameGenerator() == %v, want %s", got, want)
+		}
+	})
+}
