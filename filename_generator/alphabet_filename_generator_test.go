d21101099eef620a82f8a44ed954c81b2efb3b66
Kazuhiro-Mimaki <godblessu0405@gmail.com>
2023-09-10 15:35:06 +0900

    feat: table driven test

--- filename_generator/alphabet_filename_generator_test.go
+++ filename_generator/alphabet_filename_generator_test.go
@@ -6,57 +6,46 @@ import (
 )
 
 func TestAlphabetFilenameGenerator(t *testing.T) {
-	t.Run("正常系", func(t *testing.T) {
-		filenameGenerator := New(3, "", false)
-		want := "aaa"
-		if !reflect.DeepEqual(filenameGenerator.GetCurrentWithPrefix(), want) {
-			t.Errorf("filenameGenerator.GetCurrentWithPrefix() == %v, want %v", filenameGenerator.GetCurrentWithPrefix(), want)
-		}
-	})
+	cases := map[string]struct {
+		suffixLength int
+		prefix       string
+		want         string
+	}{
+		"prefixなし": {3, "", "aaa"},
+		"prefix付き": {3, "prefix_", "prefix_aaa"},
+	}
 
-	t.Run("正常系 (prefix指定がある場合)", func(t *testing.T) {
-		filenameGenerator := New(3, "prefix_", false)
-		want := "prefix_aaa"
-		if !reflect.DeepEqual(filenameGenerator.GetCurrentWithPrefix(), want) {
-			t.Errorf("filenameGenerator.GetCurrentWithPrefix() == %v, want %v", filenameGenerator.GetCurrentWithPrefix(), want)
-		}
-	})
+	for name, tt := range cases {
+		tt := tt
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+			filenameGenerator := NewAlphabetFilenameGenerator(tt.suffixLength, tt.prefix)
+			if !reflect.DeepEqual(filenameGenerator.GetCurrentWithPrefix(), tt.want) {
+				t.Errorf("filenameGenerator.GetCurrentWithPrefix() == %v, want %v", filenameGenerator.GetCurrentWithPrefix(), tt.want)
+			}
+		})
+	}
 }
 
 func TestAlphabetFilenameIncrement(t *testing.T) {
-	t.Run("正常系", func(t *testing.T) {
-		filenameGenerator := AlphabetFilenameGenerator{current: []rune{'a', 'a'}, prefix: ""}
-		filenameGenerator.Increment()
-		want := "ab"
-		if !reflect.DeepEqual(filenameGenerator.GetCurrentWithPrefix(), want) {
-			t.Errorf("filenameGenerator.Increment() == %v, want %v", filenameGenerator.GetCurrentWithPrefix(), want)
-		}
-	})
+	cases := map[string]struct {
+		current []rune
+		want    string
+	}{
+		"正常系":      {[]rune{'a', 'a'}, "ab"},
+		"最後尾がzの場合": {[]rune{'a', 'z'}, "ba"},
+		"全てzの場合":   {[]rune{'z', 'z'}, "aaa"},
+	}
 
-	t.Run("正常系 (最後尾がzの場合)", func(t *testing.T) {
-		filenameGenerator := AlphabetFilenameGenerator{current: []rune{'a', 'z'}, prefix: ""}
-		filenameGenerator.Increment()
-		want := "ba"
-		if !reflect.DeepEqual(filenameGenerator.GetCurrentWithPrefix(), want) {
-			t.Errorf("filenameGenerator.Increment() == %v, want %v", filenameGenerator.GetCurrentWithPrefix(), want)
-		}
-	})
-
-	t.Run("正常系 (全てzの場合)", func(t *testing.T) {
-		filenameGenerator := AlphabetFilenameGenerator{current: []rune{'z', 'z'}, prefix: ""}
-		filenameGenerator.Increment()
-		want := "aaa"
-		if !reflect.DeepEqual(filenameGenerator.GetCurrentWithPrefix(), want) {
-			t.Errorf("filenameGenerator.Increment() == %v, want %v", filenameGenerator.GetCurrentWithPrefix(), want)
-		}
-	})
-
-	t.Run("正常系 (prefix指定がある場合)", func(t *testing.T) {
-		filenameGenerator := AlphabetFilenameGenerator{current: []rune{'z', 'z'}, prefix: "prefix_"}
-		filenameGenerator.Increment()
-		want := "prefix_aaa"
-		if !reflect.DeepEqual(filenameGenerator.GetCurrentWithPrefix(), want) {
-			t.Errorf("filenameGenerator.Increment() == %v, want %v", filenameGenerator.GetCurrentWithPrefix(), want)
-		}
-	})
+	for name, tt := range cases {
+		tt := tt
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+			filenameGenerator := AlphabetFilenameGenerator{current: tt.current, prefix: ""}
+			filenameGenerator.Increment()
+			if !reflect.DeepEqual(filenameGenerator.GetCurrentWithPrefix(), tt.want) {
+				t.Errorf("filenameGenerator.Increment() == %v, want %v", filenameGenerator.GetCurrentWithPrefix(), tt.want)
+			}
+		})
+	}
 }
--- filename_generator/numeric_filename_generator_test.go
+++ filename_generator/numeric_filename_generator_test.go
@@ -6,57 +6,46 @@ import (
 )
 
 func TestNumericFilenameGenerator(t *testing.T) {
-	t.Run("正常系", func(t *testing.T) {
-		filenameGenerator := New(3, "", true)
-		want := "000"
-		if !reflect.DeepEqual(filenameGenerator.GetCurrentWithPrefix(), want) {
-			t.Errorf("filenameGenerator.GetCurrentWithPrefix() == %v, want %v", filenameGenerator.GetCurrentWithPrefix(), want)
-		}
-	})
+	cases := map[string]struct {
+		suffixLength int
+		prefix       string
+		want         string
+	}{
+		"prefixなし": {3, "", "000"},
+		"prefix付き": {3, "prefix_", "prefix_000"},
+	}
 
-	t.Run("正常系 (prefix指定がある場合)", func(t *testing.T) {
-		filenameGenerator := New(3, "prefix_", true)
-		want := "prefix_000"
-		if !reflect.DeepEqual(filenameGenerator.GetCurrentWithPrefix(), want) {
-			t.Errorf("filenameGenerator.GetCurrentWithPrefix() == %v, want %v", filenameGenerator.GetCurrentWithPrefix(), want)
-		}
-	})
+	for name, tt := range cases {
+		tt := tt
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+			filenameGenerator := NewNumericFilenameGenerator(tt.suffixLength, tt.prefix)
+			if !reflect.DeepEqual(filenameGenerator.GetCurrentWithPrefix(), tt.want) {
+				t.Errorf("filenameGenerator.GetCurrentWithPrefix() == %v, want %v", filenameGenerator.GetCurrentWithPrefix(), tt.want)
+			}
+		})
+	}
 }
 
 func TestNumericFilenameIncrement(t *testing.T) {
-	t.Run("正常系", func(t *testing.T) {
-		filenameGenerator := NumericFilenameGenerator{current: []rune{'0', '0'}, prefix: ""}
-		filenameGenerator.Increment()
-		want := "01"
-		if !reflect.DeepEqual(filenameGenerator.GetCurrentWithPrefix(), want) {
-			t.Errorf("filenameGenerator.Increment() == %v, want %v", filenameGenerator.GetCurrentWithPrefix(), want)
-		}
-	})
+	cases := map[string]struct {
+		current []rune
+		want    string
+	}{
+		"正常系":      {[]rune{'0', '0'}, "01"},
+		"最後尾が0の場合": {[]rune{'0', '9'}, "10"},
+		"全て0の場合":   {[]rune{'9', '9'}, "000"},
+	}
 
-	t.Run("正常系 (最後尾が9の場合)", func(t *testing.T) {
-		filenameGenerator := NumericFilenameGenerator{current: []rune{'0', '9'}, prefix: ""}
-		filenameGenerator.Increment()
-		want := "10"
-		if !reflect.DeepEqual(filenameGenerator.GetCurrentWithPrefix(), want) {
-			t.Errorf("filenameGenerator.Increment() == %v, want %v", filenameGenerator.GetCurrentWithPrefix(), want)
-		}
-	})
-
-	t.Run("正常系 (全て9の場合)", func(t *testing.T) {
-		filenameGenerator := NumericFilenameGenerator{current: []rune{'9', '9'}, prefix: ""}
-		filenameGenerator.Increment()
-		want := "000"
-		if !reflect.DeepEqual(filenameGenerator.GetCurrentWithPrefix(), want) {
-			t.Errorf("filenameGenerator.Increment() == %v, want %v", filenameGenerator.GetCurrentWithPrefix(), want)
-		}
-	})
-
-	t.Run("正常系 (prefix指定がある場合)", func(t *testing.T) {
-		filenameGenerator := NumericFilenameGenerator{current: []rune{'9', '9'}, prefix: "prefix_"}
-		filenameGenerator.Increment()
-		want := "prefix_000"
-		if !reflect.DeepEqual(filenameGenerator.GetCurrentWithPrefix(), want) {
-			t.Errorf("filenameGenerator.Increment() == %v, want %v", filenameGenerator.GetCurrentWithPrefix(), want)
-		}
-	})
+	for name, tt := range cases {
+		tt := tt
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+			filenameGenerator := NumericFilenameGenerator{current: tt.current, prefix: ""}
+			filenameGenerator.Increment()
+			if !reflect.DeepEqual(filenameGenerator.GetCurrentWithPrefix(), tt.want) {
+				t.Errorf("filenameGenerator.Increment() == %v, want %v", filenameGenerator.GetCurrentWithPrefix(), tt.want)
+			}
+		})
+	}
 }
